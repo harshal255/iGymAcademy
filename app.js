c73cb2327001992aae50aba45e0eb43fc57c0d31
Harshal <harshalskahar389@gmail.com>
2022-09-22 14:52:17 +0530

    mongodb & mongoose

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ const app = express();
 const port = 80;
 const path = require("path");
 const fs = require("fs");
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+mongoose.connect('mongodb://localhost/contactgym', { useNewUrlParser: true });
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    //   console.log("we are connected");
+})
+
+//create a schema
+let contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    feedback: String
+
+});
+
+//finally compiled schema compiled (loked)
+const Contact = mongoose.model('Contact', contactSchema);
 
 
 
@@ -47,27 +68,33 @@ app.get("/services", (req, res) => {
 
 })
 
-app.post("/contact",(req,res)=>{
-    const param1 = { "message": 'Your form is submitted successfully'};
+app.post("/contact", (req, res) => {
+    const param1 = { "message": 'Your form is submitted successfully' };
     name1 = req.body.name;
     phone = req.body.phone;
     email = req.body.email;
     feedback = req.body.feedback;
-   
-
-    let outputToWrite = `
-    the name of the client is ${name1}, 
-    his/her mobile no is ${phone}, 
-    email is ${email}  & 
-    Given feedback is ${feedback}`
-    fs.writeFileSync('output.txt',outputToWrite);
 
 
-    
+    // let outputToWrite = `
+    // the name of the client is ${name1}, 
+    // his/her mobile no is ${phone}, 
+    // email is ${email}  & 
+    // Given feedback is ${feedback}`
+    // fs.writeFileSync('output.txt',outputToWrite);
 
-    res.status(200).render('contact.pug', param1);
+    var myData = new Contact(req.body);
+    myData.save().then(() => {
+        // location.reload(); 
+        res.send("This item has been saved to the database")
+        // alert("Thankyou for Subbmiting form..");
+    }).catch(() => {
+        res.status(400).send("item was not saved to the databse");
+        // alert("item was not saved to the databse");
+    })
+    // res.status(200).render('contact.pug', param1);
     // console.log(req.body);
-    
+
 })
 
 
